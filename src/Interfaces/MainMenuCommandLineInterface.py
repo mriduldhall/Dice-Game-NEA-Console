a65be4549f0d3e9c472d564ab1ca5880bdf2ccae
mriduldhall <mriduldhall1@gmail.com>
2021-08-22 09:59:24 +0100

    Built basic main menu structure along with game initialisation file

---
+++ src/Interfaces/MainMenuCommandLineInterface.py
@@ -0,0 +1,71 @@
+class RegisterMenuItem:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def execute():
+        print("Register(WIP)")
+
+    @staticmethod
+    def exit_initiated():
+        return False
+
+
+class LoginMenuItem:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def execute():
+        print("Login(WIP)")
+
+    @staticmethod
+    def exit_initiated():
+        return False
+
+
+class InformationMenuItem:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def execute():
+        print("No information available")
+
+    @staticmethod
+    def exit_initiated():
+        return False
+
+
+class ExitMenuItem:
+    def __init__(self):
+        self.is_exit_initiated = False
+
+    def execute(self):
+        print("Exiting...")
+        self.is_exit_initiated = True
+
+    def exit_initiated(self):
+        return self.is_exit_initiated
+
+
+class CLI:
+    def __init__(self):
+        self.main_menu_dict = {
+            'r': RegisterMenuItem(),
+            'l': LoginMenuItem(),
+            'i': InformationMenuItem(),
+            'e': ExitMenuItem(),
+        }
+
+    def initiate(self):
+        print("Welcome to The OCR NEA Task Dice Game!")
+        exit_initiated = False
+        while not exit_initiated:
+            choice = input("Enter r to register, l to login or i to get more information.\nEnter e to exit the game:").lower()
+            menu_item = self.main_menu_dict.get(choice)
+            if menu_item is None:
+                print("Enter valid choice")
+                continue
+            menu_item.execute()
+            exit_initiated = menu_item.exit_initiated()
---
+++ src/Main.py
@@ -0,0 +1,4 @@
+from Interfaces.MainMenuCommandLineInterface import CLI
+
+if __name__ == '__main__':
+    CLI().initiate()
